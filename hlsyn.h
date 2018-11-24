2f3392c181536cbf79f76c91a82e57a1d7355baf
David Tondre <44331704+dat1txn@users.noreply.github.com>
2018-11-23 23:58:56 -0700

    Add files via upload
    added header file for CMake structure. Also modified the reading input file code.

---
+++ hlsyn.h
@@ -0,0 +1,11 @@
+// hlsyn.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+// TODO: Reference additional headers your program requires here.
